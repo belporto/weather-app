e107184c5ec67c406c83ab7085143a11f685266f
Isabel Porto <belporto@gmail.com>
2016-06-22 00:22:41 -0300

    Create HomeFragment with cards

--- app/src/main/java/br/com/porto/isabel/weather/home/HomeFragment.java
+++ app/src/main/java/br/com/porto/isabel/weather/home/HomeFragment.java
@@ -1,6 +1,21 @@
 package br.com.porto.isabel.weather.home;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import br.com.porto.isabel.weather.R;
 
 public class HomeFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+
+        return inflater.inflate(R.layout.home_fragment, container, false);
+    }
 }
